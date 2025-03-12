637756cb092d626632da7b0599d588f4deb2b1ef
GUILLOTEAU David <dguil19@yahoo.fr>
2025-03-12 12:22:14 +0100

    Add log

--- scripts/mariadb-install-db.sh
+++ scripts/mariadb-install-db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ls -l
-echo ${MARIADB_ROOT_PASSWORD}
-echo ${MARIADB_DATABASE}
-echo ${MARIADB_USER}
-echo ${MARIADB_PASSWORD}
+echo "MARIADB_ROOT_PASSWORD = ${MARIADB_ROOT_PASSWORD}"
+echo "MARIADB_DATABASE = ${MARIADB_DATABASE}"
+echo "MARIADB_USER = ${MARIADB_USER}"
+echo "MARIADB_PASSWORD = ${MARIADB_PASSWORD}"
 
 sudo apt install mariadb-client
 mariadb --port 5432 --user ${MARIADB_USER} --password ${MARIADB_PASSWORD} < scripts/db/install-db-tests.sql;
