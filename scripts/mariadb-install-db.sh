19343e32ccbc955dc03c1577e8afa1f2c41e0f22
GUILLOTEAU David <dguil19@yahoo.fr>
2025-03-12 11:19:41 +0100

    Init db

--- scripts/mariadb-install-db.sh
+++ scripts/mariadb-install-db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 ls -l
-service mariadb status
-mysql < scripts/db/install-db-tests.sql
+echo ${MARIADB_ROOT_PASSWORD}
+echo ${MARIADB_DATABASE}
+echo ${MARIADB_USER}
+echo ${MARIADB_PASSWORD}
+
+mysql -u${MARIADB_USER} -p${MARIADB_PASSWORD}; create database ${MARIADB_DATABASE}; use ${MARIADB_DATABASE}; source scripts/db/install-db-tests.sql;
