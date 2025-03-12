b6af91612382b0f0138a4f854dde213c47cf096d
GUILLOTEAU David <dguil19@yahoo.fr>
2025-03-12 11:24:46 +0100

    Init db

--- scripts/mariadb-install-db.sh
+++ scripts/mariadb-install-db.sh
@@ -6,4 +6,4 @@ echo ${MARIADB_DATABASE}
 echo ${MARIADB_USER}
 echo ${MARIADB_PASSWORD}
 
-mysql -u${MARIADB_USER} -p${MARIADB_PASSWORD}; create database ${MARIADB_DATABASE}; use ${MARIADB_DATABASE}; source scripts/db/install-db-tests.sql;
+mariadb < scripts/db/install-db-tests.sql;
