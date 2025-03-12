c99c95902b3458bd28cd6fc69b50e5dd46330aea
GUILLOTEAU David <dguil19@yahoo.fr>
2025-03-12 14:33:11 +0100

    Nettoyage

--- scripts/db/install-db-tests.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE DATABASE `les-jardins-de-la-noue` /*!40100 COLLATE 'utf8mb4_general_ci' */
-CREATE USER 'LesJardinsDeLaNoue'@'localhost' IDENTIFIED VIA mysql_native_password USING 'ljdln@2025';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER ON *.* TO 'LesJardinsDeLaNoue'@'localhost'; 
